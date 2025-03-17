76b018c39d674ebeb0cfb8c13cfc54800c492f84
Sivakumar Balasubramanian <siva82kb@gmail.com>
2025-03-17 18:49:53 +0530

    First commit.

---
+++ .eleventy.js
@@ -0,0 +1,25 @@
+module.exports = function(eleventyConfig) {
+    // Tell Eleventy to copy the assets folder to `_site`
+    eleventyConfig.addPassthroughCopy("src/assets");
+  
+    // Define the "posts" collection that will contain all blogposts
+    eleventyConfig.addCollection("posts", function(collectionApi) {
+      return collectionApi.getFilteredByGlob("src/posts/*.md").reverse();
+    });
+  
+    eleventyConfig.addFilter("date", function (date, format) {
+      const { DateTime } = require("luxon");
+      return DateTime.fromJSDate(date).toFormat(format);
+    });
+  
+    // Tell Eleventy where layouts are
+    return {
+      dir: {
+        input: "src",
+        output: "_site"
+      },
+      pathPrefix: "/informatics_club/",
+    };
+  };
+  
+  
